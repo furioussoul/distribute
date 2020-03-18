c3aa020508d935ad0b28c1348f21c9ebddc06451
szj <sunzhengjie@cloudwalk.cn>
2020-03-18 19:24:51 +0800

    pass 3A

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -2,6 +2,7 @@ package kvraft
 
 import (
 	"../labrpc"
+	"fmt"
 	"math/rand"
 	"sync"
 	"time"
@@ -19,9 +20,47 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
 	// You'll have to add code here.
-	ck.id = rand.Intn(10)
+	ck.id = generateRandomNumber(10, 1000000, 1)[0]
+	fmt.Println(ck.id)
 	ck.seqId = 0
 	return ck
+}
+
+//生成若干个不重复的随机数
+func RandomTestBase() {
+	//测试5次
+	for i := 0; i < 5; i++ {
+		nums := generateRandomNumber(10, 30, 10)
+		fmt.Println(nums)
+	}
+}
+
+//生成count个[start,end)结束的不重复的随机数
+func generateRandomNumber(start int, end int, count int) []int {
+	//范围检查
+	if end < start || (end-start) < count {
+		return nil
+	}
+	//存放结果的slice
+	nums := make([]int, 0)
+	//随机数生成器，加入时间戳保证每次生成的随机数不一样
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for len(nums) < count {
+		//生成随机数
+		num := r.Intn((end - start)) + start
+		//查重
+		exist := false
+		for _, v := range nums {
+			if v == num {
+				exist = true
+				break
+			}
+		}
+		if !exist {
+			nums = append(nums, num)
+		}
+	}
+	return nums
 }
 
 //
